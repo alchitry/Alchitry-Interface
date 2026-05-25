060a379b6dd2f1ddc03829a79c06336a3cf0f2cf
Justin Rajewski <Justin@alchitry.com>
2026-05-25 11:43:51 -0600

    Added Mac ARM specific dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,11 +13,12 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.usb4java:usb4java:1.3.0")
     implementation("com.fazecast:jSerialComm:2.11.4")
     implementation("com.github.alchitry.yad2xx:yad2xxJava:8d48cda")
     implementation(kotlin("reflect"))
     testImplementation(kotlin("test"))
+    implementation("io.github.dsheirer:usb4java:1.3.5")
+    implementation("io.github.dsheirer:usb4java-native-libraries:1.3.3")
 }
 
 kotlin {
